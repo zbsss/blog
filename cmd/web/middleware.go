2659d496e7594a18410e0cc7f475ebbe6b675bc1
mkurleto <michal.kurleto@gmail.com>
2024-03-31 13:11:13 +0200

    temporarily disable CSP; update hugo build

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -7,8 +7,9 @@ import (
 
 func secureHeaders(next http.Handler) http.Handler {
 	middleware := func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Security-Policy",
-			"default-src 'self'; style-src 'self' fonts.googleapis.com; font-src fonts.gstatic.com")
+		// TODO: Temporarily disabled Content-Security-Policy
+		// w.Header().Set("Content-Security-Policy",
+		// 	"default-src 'self'; style-src 'self' fonts.googleapis.com; font-src fonts.gstatic.com")
 		w.Header().Set("Referrer-Policy", "origin-when-cross-origin")
 		w.Header().Set("X-Content-Type-Options", "nosniff")
 		w.Header().Set("X-Frame-Options", "deny")
